c8e9c1adc117482df93ec6f3eec143b7d49f870a
Dmitriy Dihtiarov <dmitriydegtyarev1985@gmail.com>
2021-05-31 00:39:20 +0300

    fix fetchCountries and delete import and use axios

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,32 +1,7 @@
-// import axios from 'axios';
-
 const baseUrl = 'https://restcountries.eu/rest/v2/name/';
-// axios.defaults.baseURL = `https://restcountries.eu/rest/v2/name/`;
 
-export default function fetchCountries(searchQuery) {
-
-//   return axios.get(searchQuery)
-//     .then(response => {
-//       console.log('than');
-//       console.log(response.data);
-//       return response.data;
-//     })
-//     .catch(error => {
-//       console.log('error');
-//       console.error(error);
-// });      
+export default function fetchCountries(searchQuery) {    
   
   return fetch(baseUrl + searchQuery)
     .then(response => response.json());
-  
-    // .then(response => {
-      // console.log('then1');
-      // console.log(response);
-    //   return response.json();
-    // });
-    // .then(data => {
-    //   console.log('then2');
-    //   console.log(data);
-    //   return data;
-    // });
 };
